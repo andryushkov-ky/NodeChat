403c0638ee2c2eefcfe82eaad7047191f524e46f
Andryushkov-ky <ka@madrobots.ru>
2017-09-20 12:52:12 +0700

    [master] Init prj

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
-/**
- * Created by Andryushkov-ky on 20.09.2017.
- */
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server on port ${port}`)
+});
