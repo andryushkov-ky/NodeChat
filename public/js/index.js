35c7ddd6b6c4a213ec33f002f182692666452bbf
Andryushkov-ky <ka@madrobots.ru>
2017-09-21 11:41:16 +0700

    [master] sending message

---
+++ public/js/index.js
@@ -0,0 +1,29 @@
+const socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server')
+});
+
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
+
+    const li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
+});
+
+$('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'Frank',
+        text: $('[name=message]').val()
+    }, function (data) {
+        console.log('Got it', data);
+    });
+});
