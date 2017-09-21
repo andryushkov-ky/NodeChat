2b93dba58e6c97f25b79e19437b345c0c7261baa
Andryushkov-ky <ka@madrobots.ru>
2017-09-21 12:35:05 +0700

    [master] add button with geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -27,3 +27,20 @@ $('#message-form').on('submit', function (e) {
         console.log('Got it', data);
     });
 });
+
+const locationButton = $('#send-location');
+
+locationButton.on('click', function() {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, function () {
+        alert('Unable to fetch location.')
+    })
+});
