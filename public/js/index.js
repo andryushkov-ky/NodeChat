17e1ad551f9ef540f1e852f390517d038557ea51
Andryushkov-ky <ka@madrobots.ru>
2017-09-22 09:47:15 +0700

    [master] add moment js

--- public/js/index.js
+++ public/js/index.js
@@ -9,19 +9,20 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('newMessage', message);
+    const formattedtime = moment(message.createdAt).format('h:mm a');
 
     const li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedtime}: ${message.text}`);
 
     $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
+    const formattedtime = moment(message.createdAt).format('h:mm a');
     let li = $('<li></li>');
     let a = $('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedtime}: `);
     a.attr('href', message.url);
     li.append(a);
     $('#messages').append(li);
